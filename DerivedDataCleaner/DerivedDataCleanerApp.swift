a641dddd3c4e9ba8ebc9166d40a09666477c80e6
jambo <jambosucks@gmail.com>
2024-11-14 04:27:21 -0600

    Initial Commit

---
+++ DerivedDataCleaner/DerivedDataCleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DerivedDataCleanerApp.swift
+//  DerivedDataCleaner
+//
+//  Created by jambo on 11/14/24.
+//
+
+import SwiftUI
+
+@main
+struct DerivedDataCleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DerivedDataCleanerTests/DerivedDataCleanerTests.swift
@@ -0,0 +1,17 @@
+//
+//  DerivedDataCleanerTests.swift
+//  DerivedDataCleanerTests
+//
+//  Created by jambo on 11/14/24.
+//
+
+import Testing
+@testable import DerivedDataCleaner
+
+struct DerivedDataCleanerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
